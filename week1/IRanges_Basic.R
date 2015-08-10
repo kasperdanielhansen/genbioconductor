71141dd2fa8f0a19719f267f9e97979c5dfdf489
Kasper Daniel Hansen <kasperdanielhansen@gmail.com>
2015-08-10 14:36:01 -0400

    switching to release R

---
+++ week1/IRanges_Basic.R
@@ -0,0 +1,119 @@
+## ----front, child="front.Rmd", echo=FALSE--------------------------------
+
+## ----global_options, echo=FALSE------------------------------------------
+knitr::opts_chunk$set(fig.width=5, fig.height=5, size="footnotesize",
+                      warning=FALSE, message=FALSE)
+knitr::knit_hooks$set(small.mar = function(before, options, envir) {
+  if (before) graphics::par(mar = c(5,5,1.5,1))
+})
+
+## ----standardPackages, echo=FALSE, message=FALSE-------------------------
+## require("knitcitations")
+## cleanbib()
+
+## ----plotRanges, echo=FALSE----------------------------------------------
+plotRanges <- function(x, xlim = x, main = deparse(substitute(x)),
+                       col = "black", sep = 0.5, ...) {
+    height <- 1
+    if (is(xlim, "Ranges"))
+        xlim <- c(min(start(xlim)), max(end(xlim)))
+    bins <- disjointBins(IRanges(start(x), end(x) + 1))
+    plot.new()
+    plot.window(xlim, c(0, max(bins)*(height + sep)))
+    ybottom <- bins * (sep + height) - height
+    rect(start(x)-0.5, ybottom, end(x)+0.5, ybottom + height, col = col, ...)
+    title(main)
+    axis(1)
+}
+
+
+## ----dependencies, warning=FALSE, message=FALSE--------------------------
+library(IRanges)
+
+## ----biocLite, eval=FALSE------------------------------------------------
+## source("http://www.bioconductor.org/biocLite.R")
+## biocLite(c("IRanges"))
+
+## ----iranges1------------------------------------------------------------
+ir1 <- IRanges(start = c(1,3,5), end = c(3,5,7))
+ir1
+ir2 <- IRanges(start = c(1,3,5), width = 3)
+all.equal(ir1, ir2)
+
+## ----ir_width------------------------------------------------------------
+start(ir1)
+width(ir2) <- 1
+ir2
+
+## ----ir_names------------------------------------------------------------
+names(ir1) <- paste("A", 1:3, sep = "")
+ir1
+
+## ----ir_dim--------------------------------------------------------------
+dim(ir1)
+length(ir1)
+
+## ----ir_subset-----------------------------------------------------------
+ir1[1]
+ir1["A1"]
+
+## ---- echo=FALSE---------------------------------------------------------
+ir <- IRanges(start = c(1,3,7,9), end = c(4,4,8,10))
+
+## ---- echo=FALSE, fig.height=2, small.mar=TRUE---------------------------
+plotRanges(ir)
+
+## ---- echo=FALSE, fig.height=1.75, small.mar=TRUE------------------------
+plotRanges(reduce(ir))
+
+## ------------------------------------------------------------------------
+ir
+reduce(ir)
+
+## ---- eval=FALSE---------------------------------------------------------
+## disjoin(ir1)
+
+## ---- echo=FALSE, fig.height=2, small.mar=TRUE---------------------------
+plotRanges(ir)
+
+## ---- echo=FALSE, fig.height=1.75, small.mar=TRUE------------------------
+plotRanges(disjoin(ir))
+
+## ----ir_resize-----------------------------------------------------------
+resize(ir, width = 1, fix = "start")
+resize(ir, width = 1, fix = "end")
+
+## ----ir_sets-------------------------------------------------------------
+ir1 <- IRanges(start = c(1, 3, 5), width = 1)
+ir2 <- IRanges(start = c(4, 5, 6), width = 1)
+union(ir1, ir2)
+intersect(ir1, ir2)
+
+## ----findOverlaps--------------------------------------------------------
+ir1 <- IRanges(start = c(1,4,8), end = c(3,7,10))
+ir2 <- IRanges(start = c(3,4), width = 3)
+ov <- findOverlaps(ir1, ir2)
+ov
+
+## ----findOverlaps_ill----------------------------------------------------
+intersect(ir1[subjectHits(ov)[1]],
+          ir2[queryHits(ov)[2]])
+
+## ----subjectHits---------------------------------------------------------
+subjectHits(ov)
+unique(subjectHits(ov))
+
+## ---- tidy=TRUE----------------------------------------------------------
+args(findOverlaps)
+
+## ----countOverlaps-------------------------------------------------------
+countOverlaps(ir1, ir2)
+
+## ------------------------------------------------------------------------
+ir1
+ir2
+nearest(ir1, ir2)
+
+## ---- echo=FALSE---------------------------------------------------------
+sessionInfo()
+
