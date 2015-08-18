49ffe36574571c69ae4091cd696e635670da4a04
Kasper Daniel Hansen <kasperdanielhansen@gmail.com>
2015-08-17 21:23:58 -0400

    split two vignettes into four

--- week2/BSgenome_Views.R
+++ week2/BSgenome_Views.R
@@ -1,54 +1,16 @@
-## ----front, child="front.Rmd", echo=FALSE--------------------------------
-
-## ----global_options, echo=FALSE------------------------------------------
-knitr::opts_chunk$set(fig.width=5, fig.height=5, size="footnotesize",
-                      warning=FALSE, message=FALSE)
-knitr::knit_hooks$set(small.mar = function(before, options, envir) {
-  if (before) graphics::par(mar = c(5,5,1.5,1))
-})
-
-## ----standardPackages, echo=FALSE, message=FALSE-------------------------
-## require("knitcitations")
-## cleanbib()
-
-## ----plotRanges, echo=FALSE----------------------------------------------
-plotRanges <- function(x, xlim = x, main = deparse(substitute(x)),
-                       col = "black", sep = 0.5, ...) {
-    height <- 1
-    if (is(xlim, "Ranges"))
-        xlim <- c(min(start(xlim)), max(end(xlim)))
-    bins <- disjointBins(IRanges(start(x), end(x) + 1))
-    plot.new()
-    plot.window(xlim, c(0, max(bins)*(height + sep)))
-    ybottom <- bins * (sep + height) - height
-    rect(start(x)-0.5, ybottom, end(x)+0.5, ybottom + height, col = col, ...)
-    title(main)
-    axis(1)
-}
-
-
 ## ----dependencies, warning=FALSE, message=FALSE--------------------------
-library(Biostrings)
 library(BSgenome)
 library(BSgenome.Scerevisiae.UCSC.sacCer2)
 library(AnnotationHub)
 
 ## ----biocLite, eval=FALSE------------------------------------------------
 ## source("http://www.bioconductor.org/biocLite.R")
-## biocLite(c("Biostrings", "BSgenome",
+## biocLite(c("BSgenome",
 ##            "BSgenome.Scerevisiae.UCSC.sacCer2", "AnnotationHub"))
 
-## ----mmatchPattern-------------------------------------------------------
-dnaseq <- DNAString("ACGTACGT")
-matchPattern(dnaseq, Scerevisiae$chrI)
-countPattern(dnaseq, Scerevisiae$chrI)
-vmatchPattern(dnaseq, Scerevisiae)
-head(vcountPattern(dnaseq, Scerevisiae))
-
-## ----revCompCheck--------------------------------------------------------
-dnaseq == reverseComplement(dnaseq)
-
 ## ----views1--------------------------------------------------------------
+library("BSgenome.Scerevisiae.UCSC.sacCer2")
+dnaseq <- DNAString("ACGTACGT")
 vi <- matchPattern(dnaseq, Scerevisiae$chrI)
 vi
 
@@ -96,6 +58,6 @@ gcPercentage
 plot(density(gcProm))
 abline(v = gcPercentage, col = "red")
 
-## ---- echo=FALSE---------------------------------------------------------
+## ----sessionInfo, echo=FALSE---------------------------------------------
 sessionInfo()
 
