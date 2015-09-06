159473443d55634a82094c996637c230a60d0274
Kasper Daniel Hansen <kasperdanielhansen@gmail.com>
2015-09-06 19:32:36 -0400

    spliting an Rmd

--- in_prep/Rsamtools.R
+++ in_prep/Rsamtools.R
@@ -1,29 +1,9 @@
 ## ----dependencies, warning=FALSE, message=FALSE--------------------------
 library(Rsamtools)
-library(ShortRead)
 
 ## ----biocLite, eval=FALSE------------------------------------------------
 ## source("http://www.bioconductor.org/biocLite.R")
-## biocLite(c("Rsamtools", "ShortRead"))
-
-## ----fastq1--------------------------------------------------------------
-fastqDir <- system.file("extdata", "E-MTAB-1147", package = "ShortRead")
-fastqPath <- list.files(fastqDir, pattern = ".fastq.gz$", full = TRUE)[1]
-reads <- readFastq(fastqPath)
-reads
-
-## ----fastq2--------------------------------------------------------------
-fqFile <- FastqFile(fastqPath)
-fqFile
-reads <- readFastq(fqFile)
-
-## ----accessorFastq-------------------------------------------------------
-sread(reads)[1:2]
-quality(reads)[1:2]
-id(reads)[1:2]
-
-## ----convertQual---------------------------------------------------------
-as(quality(reads), "matrix")[1:2,1:10]
+## biocLite(c("Rsamtools")
 
 ## ----bamPath-------------------------------------------------------------
 bamPath <- system.file("extdata", "ex1.bam", package="Rsamtools")
@@ -60,7 +40,7 @@ aln <- scanBam(bamFile, param = params)
 names(aln)
 head(aln[[1]]$pos)
 
-## ------------------------------------------------------------------------
+## ----summary-------------------------------------------------------------
 quickBamFlagSummary(bamFile)
 
 ## ----BamViews------------------------------------------------------------
---
+++ in_prep/ShortRead.R
@@ -0,0 +1,29 @@
+## ----dependencies, warning=FALSE, message=FALSE--------------------------
+library(ShortRead)
+
+## ----biocLite, eval=FALSE------------------------------------------------
+## source("http://www.bioconductor.org/biocLite.R")
+## biocLite(c("ShortRead"))
+
+## ----fastq1--------------------------------------------------------------
+fastqDir <- system.file("extdata", "E-MTAB-1147", package = "ShortRead")
+fastqPath <- list.files(fastqDir, pattern = ".fastq.gz$", full = TRUE)[1]
+reads <- readFastq(fastqPath)
+reads
+
+## ----fastq2--------------------------------------------------------------
+fqFile <- FastqFile(fastqPath)
+fqFile
+reads <- readFastq(fqFile)
+
+## ----accessorFastq-------------------------------------------------------
+sread(reads)[1:2]
+quality(reads)[1:2]
+id(reads)[1:2]
+
+## ----convertQual---------------------------------------------------------
+as(quality(reads), "matrix")[1:2,1:10]
+
+## ----sessionInfo, echo=FALSE---------------------------------------------
+sessionInfo()
+
